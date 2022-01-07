b3fdc791dfd15ad5d86d1044f1e07ec59b9758eb
Jineh <lst5518@naver.com>
2022-01-08 03:59:00 +0900

    marker detail page

---
+++ deskterior/src/main/java/com/deker/mkt/model/ProductDetailModel.java
@@ -0,0 +1,26 @@
+package com.deker.mkt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ProductDetailModel {
+
+    private String mktProductId;
+    private String productName;
+    private int productPrice;
+    private String option1;
+    private String option1Data;
+    private String option2;
+    private String option2Data;
+    private int productQuantity;
+    private String productExplain;
+
+
+
+
+
+
+}
